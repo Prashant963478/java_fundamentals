cc9bd8243284b6526f0fdcc5bbb816db4f6b4869
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-10 11:15:17 +0530

    first commit

---
+++ farenhiet.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class farenhiet{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int celsius=sc.nextInt();
+int Fahrenheit = ((celsius * (9/5)) + 32);
+System.out.println(Fahrenheit);
+}
+}
