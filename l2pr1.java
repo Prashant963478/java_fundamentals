768dbc3fcad6041c75720bb5f29923613f0abd92
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-11 12:08:42 +0530

    calculator

---
+++ l2pr1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class l2pr1{
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    float num1=sc.nextFloat();
+    float num2=sc.nextFloat();
+    System.out.println("Add:"+(num1+num2));
+    System.out.println("Sub:"+(num1-num2));
+    System.out.println("Mul:"+(num1*num2));
+    System.out.println("Div:"+(num1/num2));
+ }
+}
