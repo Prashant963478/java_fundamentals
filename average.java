355354a76d7d6b88c5f11673392acbb5160fd7e0
Prashnt Solanki <95557541+Prashant963478@users.noreply.github.com>
2025-06-11 11:23:18 +0530

    second

---
+++ average.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class average{
+public static void main(String[] args){
+   int maths=94;
+   int physics=95;
+   int chemistry=96;
+   int average=(maths + physics + chemistry) / 3;
+   System.out.println(average);
+ }
+}
